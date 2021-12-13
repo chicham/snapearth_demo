4533913ebf9181fd6889bf852d3dd2a01450aea4
Hicham Randrianarivo <hicham+github@randrianarivo.net>
2021-12-13 21:03:21 +0100

    Add ipywidgets

--- demo.py
+++ demo.py
@@ -8,6 +8,10 @@
 #       format_name: light
 #       format_version: '1.5'
 #       jupytext_version: 1.13.4
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
 # ---
 
 import asyncio
